e02fdf3676789b0f6caaf32c2962fae97994b675
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-01 14:58:06 +0100

    add. new variable

--- mail/js/script.js
+++ mail/js/script.js
@@ -7,10 +7,16 @@ stampa un messaggio appropriato sull’esito del controllo.*/
 
 var listaMail = ["a.fico@gmail.com", "fico@hotmail.com", "alfred@yahoo.com"];
 
-console.log(listaMail);
 
-// 2 prompt per richiedere mail all'utente
+
+// 2 creo variabile e prompt per richiedere mail all'utente
+
+var mailUtente = prompt("Inserisci la tua mail");
+
+
 
 // 3 verifico che la mail sia presente in lista
+
+
 
 // 4 stampo messaggio sull'esito
