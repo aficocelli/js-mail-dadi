9995e84bfdac6118ba42651e83322a288e8ac7d6
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-01 14:55:09 +0100

    add. new array

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,1 +1,16 @@
+/*Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.*/
 
+
+// 1 creo array contenente una lista di Mail
+
+var listaMail = ["a.fico@gmail.com", "fico@hotmail.com", "alfred@yahoo.com"];
+
+console.log(listaMail);
+
+// 2 prompt per richiedere mail all'utente
+
+// 3 verifico che la mail sia presente in lista
+
+// 4 stampo messaggio sull'esito
